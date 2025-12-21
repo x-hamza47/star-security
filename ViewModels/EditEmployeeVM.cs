38e97a9892cc87a1c73df003e698b6bc18d51514
Hamza <hamza47@gmail.com>
2025-12-21 18:35:15 +0500

    Profile

---
+++ ViewModels/EditEmployeeVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Star_Security.ViewModels
+{
+    public class EditEmployeeVM
+    {
+        public string Id { get; set; }
+        public string EmpCode { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int? DepartmentId { get; set; }
+        public int? GradeId { get; set; }
+    }
+}
